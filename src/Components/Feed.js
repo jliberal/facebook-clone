49dfc6b37622cbcb79dd90d9a82f00a7b6cd03b4
John Paul Liberal <jliberal@gmail.com>
2020-08-30 17:32:58 -0400

    Post & Posts

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import '../Styles/Feed.css';
 import StoryReel from './StoryReel';
+import MessageSender from './MessageSender';
+import Post from './Post';
 
 function Feed() {
 	return (
 		<div className="feed">
 			<StoryReel />
-			{/* Story Reel */}
-			{/* Message Sender */}
+			<MessageSender />
+
+			<Post
+				key="001"
+				profilePic="https://avatars.services.sap.com/images/john.liberal2.png"
+				message="Cool, estamos funcionando"
+				timestamp="Aquí va el timestamp"
+				username="johnplex"
+				image="https://3.bp.blogspot.com/_AnUh46tmTrA/R2glq-f1bpI/AAAAAAAAAB0/f-FjJKwyvlI/w1200-h630-p-k-no-nu/Blog+Zip+Compress.JPG"
+			/>
+			<Post
+				key="002"
+				profilePic="https://avatars.services.sap.com/images/john.liberal2.png"
+				message="F*ck 2020"
+				timestamp="Aquí va el timestamp"
+				username="johnplex"
+				image="https://pbs.twimg.com/media/EVT4J7VXYAE6olm.jpg"
+			/>
+			<Post
+				key="003"
+				profilePic="https://avatars.services.sap.com/images/john.liberal2.png"
+				message="Hello World"
+				timestamp="Aquí va el timestamp"
+				username="johnplex"
+			/>
 		</div>
 	);
 }
